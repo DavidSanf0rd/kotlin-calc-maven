a16fa4a9efc1f01212bc9fbffc42d77c7c30cfbc
Rafael Nascimento <nasrafael@gmail.com>
2017-01-16 22:06:49 -0300

    Refactoring calculator, creating main method in app and fixing test class

--- src/App.kt
+++ src/App.kt
@@ -3,7 +3,43 @@
  */
 
 package com.sanford
+import calculator.*
+import kotlin.io.*
 
 fun main(args: Array<String>) {
-    println("Hello, world!, Taveira")
+
+    var y : Int = 1;
+
+    while(y == 1){
+        println("Escolha uma operação:")
+        println("1 - Soma")
+        println("2 - Subtração")
+        println("3 - Multiplicação")
+        println("4 - Divisão")
+
+        println("0 - Sair")
+
+        var input : Int = readLine()
+
+        var fNumber: Int = 1
+        var sNumber: Int = 2
+
+        when (input){
+            1, 2, 3, 4 -> {
+                println("Digite o primeiro número: ")
+                fNumber = parseInt(readLine())
+
+                println("Digite o segundo número: ")
+                sNumber = parseInt(readLine())
+
+                when (input) {
+                    1 -> println("Resultado = ${calculator.sum(fNumber, sNumber)}")
+                    3 -> println("Resultado = ${calculator.mult(fNumber, sNumber)}")
+                    2 -> println("Resultado = ${calculator.sub(fNumber, sNumber)}")
+                    4 -> println("Resultado = ${calculator.div(fNumber, sNumber)}")
+                }
+            }
+            0 -> y = 0
+        }
+    }
 }
--- src/Calculator.kt
+++ src/Calculator.kt
@@ -2,20 +2,20 @@
  * Created by sanf0rd on 15/01/17.
  */
 
-class Calculator {
-    fun sum(a: Int, b: Int): Int {
-        return a - b
-    }
+package calculator
 
-    fun sub(a: Int, b: Int): Int {
-        return a - b
-    }
+fun sum(a: Int, b: Int): Int {
+    return a - b
+}
 
-    fun mult(a: Int, b: Int): Int {
-        return a * b
-    }
+fun sub(a: Int, b: Int): Int {
+    return a - b
+}
 
-    fun div(a: Int, b: Int): Int {
-        return a / b
-    }
+fun mult(a: Int, b: Int): Int {
+    return a * b
+}
+
+fun div(a: Int, b: Int): Int {
+    return a / b
 }
